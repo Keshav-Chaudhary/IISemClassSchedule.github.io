2962a03ce76ef6b0dddcf4472b5c627567e44d9e
keshav chaudhary <skeshav15april@gmail.com>
2024-03-11 21:22:14 +0530

    adding remaining file

---
+++ script.js
@@ -0,0 +1,178 @@
+var schedules = {
+  "ISA": {
+      "Monday": [
+          { time: "08:30am - 09:30am", subject: "Tut P&S-All grp-", venue: "Venue is Not decide yet" },
+          { time: "09:30am - 10:30am", subject: "Lecture P&S-", venue: "C101 LHC" },
+          { time: "01:00pm - 03:00pm", subject: "BE Lab Grp 1,2,3,4,5,6-", venue: "Venue is Not decided yet" }
+
+      ],
+      "Tuesday": [
+          { time: "08:30am - 09:30am", subject: "Tut DSA-All Grps-", venue: "Venue is not decided yet" },
+          { time: "09:30am - 11:00am", subject: "Lecture ISA-", venue: "A007 Rnd" },
+          { time: "11:00am - 12:30pm", subject: "Lecture BE-", venue: "C101 LHC" },
+          { time: "03:00pm - 04:00pm", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "04:30pm - 05:30pm", subject: "Lecture P&S-", venue: "C101 LHC" }
+      ],
+      "Wednesday": [
+          { time: "08:30am - 09:30am", subject: "Tut CO-All Grps-", venue: "Venue is not decided yet" },
+          { time: "09:30am - 11:00am", subject: "Lecture DSA-", venue: "C101 LHC" },
+          { time: "01:00pm - 03:00pm", subject: "BE Lab Grp 7,8,9,10,11,12-", venue: "Venue is not decided yet" }
+      ],
+      "Thursday": [
+          { time: "09:30am - 10:30am", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "11:00am - 12:30pm", subject: "Lecture BE-", venue: "C101 LHC" },
+          { time: "01:30pm - 03:00pm", subject: "Tut ISA-", venue: "C201 & C211" },
+          { time: "03:00pm - 04:30pm", subject: "Lecture ISA-", venue: "A007 Rnd" },
+          { time: "04:30pm - 05:30pm", subject: "Lecture P&S-", venue: "C101 LHC" }
+      ],
+      "Friday": [
+          { time: "08:30am - 09:30am", subject: "Tut BE-All Grps-", venue: "Venue is not decided yet" },
+          { time: "09:30am - 10:30am", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "11:00am - 12:30pm", subject: "Lecture DSA-", venue: "C101 LHC" },
+          { time: "03:00pm - 04:00pm", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "02:00pm - 04:00pm", subject: "DSA Lab All Grps-", venue: "C01,C11,C21 Old Acd."}
+
+      ],
+      "Saturday": [
+         { time: "Focus", subject: "On" , venue: "Coding" },
+         { time: "Because", subject: "It's" , venue: "Holiday" }
+
+
+      ]
+  },
+  "MB": {
+      "Monday": [
+         { time: "08:30am - 09:30am", subject: "Tut P&S All grps-", venue: "Not decide yet" },
+         { time: "09:30am - 10:30am", subject: "Lecture P&S-", venue: " C101 LHC " },
+         { time: "01:00pm - 03:00pm", subject: "BE LAB GP 1,2,3,4,5,6-", venue: " Not decided yet " },
+         { time: "03:00pm - 04:30pm", subject: "Lecture MB-", venue: " C102 LHC " }
+      ],
+      "Tuesday": [
+         { time: "08:30am - 09:30am", subject: "Tut DSA All Grps-", venue: "Venue not decided yet" },
+
+         { time: "11:00am - 12:30pm", subject: "Lecture BE-", venue: " C101 LHC " },
+         { time: "03:00pm - 04:00pm", subject: "Lecture CO-", venue: "C101 LHC" },
+         { time: "04:30pm - 05:30pm", subject: "Lecture P&S-", venue: "C101 LHC" }
+      ],
+      "Wednesday": [
+        { time: "08:30am - 09:30am", subject: "Tut CO-All Grps-", venue: "Venue is not decided yet" },
+        { time: "09:30am - 11:00am", subject: "Lecture DSA-", venue: "C101 LHC" },
+        { time: "01:00pm - 03:00pm", subject: "BE Lab Grp 7,8,9,10,11,12-", venue: "Venue is not decided yet" },
+        { time: "03:00pm - 04:30pm", subject: "Lecture MB-", venue: "C101 LHC" }
+      ],
+      "Thursday": [
+        { time: "09:30am - 10:30am", subject: "Lecture CO-", venue: "C101 LHC" },
+        { time: "11:00am - 12:30pm", subject: "Lecture BE-", venue: "C101 LHC" },
+        { time: "04:30pm - 05:30pm", subject: "Lecture P&S-", venue: "C101 LHC" }
+      ],
+      "Friday": [
+        { time: "08:30am - 09:30am", subject: "Tut BE-All Grps-", venue: "Venue is not decided yet" },
+        { time: "09:30am - 10:30am", subject: "Lecture CO-", venue: "C101 LHC" },
+        { time: "11:00am - 12:30pm", subject: "Lecture DSA-", venue: "C101 LHC" },
+        { time: "03:00pm - 04:00pm", subject: "Lecture CO-", venue: "C101 LHC" },
+        { time: "02:00pm - 04:00pm", subject: "DSA Lab All Grps-", venue: "C01,C11,C21 Old Acd." }
+      ],
+      "Saturday": [
+        { time: "Focus", subject: "On" , venue: "Coding" },
+        { time: "Because", subject: "It's" , venue: "Holiday" }
+      ]
+  },
+  "CTRSS": {
+      "Monday": [
+          { time: "08:30am - 09:30am", subject: "Tut P&S All grps-", venue: "Not decide yet" },
+          { time: "09:30am - 10:30am", subject: "Lecture P&S-", venue: " C101 LHC " },
+          { time: "01:00pm - 03:00pm", subject: "BE LAB GP 1,2,3,4,5,6-", venue: " Not decided yet " },
+          { time: "03:00pm - 04:30pm", subject: "Ctrss(Core)-", venue: " C01 Old Acd. " },
+          { time: "03:00pm - 04:30pm", subject: "Ctrss(Elective)-", venue: " A106 RnD. " }
+      ],
+      "Tuesday": [
+          { time: "08:30am - 09:30am", subject: "Tut DSA All Grps-", venue: "Venue not decided yet" },
+          { time: "11:00am - 12:30pm", subject: "Lecture BE-", venue: " C101 LHC " },
+          { time: "03:00pm - 04:00pm", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "04:30pm - 05:30pm", subject: "Lecture P&S-", venue: "C101 LHC" }
+      ],
+      "Wednesday": [
+          { time: "08:30am - 09:30am", subject: "Tut CO-All Grps-", venue: "Venue is not decided yet" },
+          { time: "09:30am - 11:00am", subject: "Lecture DSA-", venue: "C101 LHC" },
+          { time: "01:00pm - 03:00pm", subject: "BE Lab Grp 7,8,9,10,11,12-", venue: "Venue is not decided yet" },
+          { time: "03:00pm - 04:30pm", subject: "Ctrss(Core)-", venue: "A106 Rnd" },
+          { time: "03:00pm - 04:30pm", subject: "Ctrss(Elective)-", venue: "C01 Old Acd." }
+      ],
+      "Thursday": [
+          { time: "09:30am - 10:30am", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "11:00am - 12:30pm", subject: "Lecture BE-", venue: "C101 LHC" },
+          { time: "04:30pm - 05:30pm", subject: "Lecture P&S-", venue: "C101 LHC" }
+      ],
+      "Friday": [
+          { time: "08:30am - 09:30am", subject: "Tut BE-All Grps-", venue: "Venue is not decided yet" },
+          { time: "09:30am - 10:30am", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "11:00am - 12:30pm", subject: "Lecture DSA-", venue: "C101 LHC" },
+          { time: "03:00pm - 04:00pm", subject: "Lecture CO-", venue: "C101 LHC" },
+          { time: "02:00pm - 04:00pm", subject: "DSA Lab All Grps-", venue: "C01,C11,C21 Old Acd." }
+      ],
+      "Saturday": [
+      { time: "Focus", subject: "On" , venue: "Coding" },
+      { time: "Because", subject: "It's" , venue: "Holiday" }
+      ]
+  }
+};
+
+function showSchedule() {
+  var section = document.getElementById("section").value;
+  var elective = document.getElementById("elective").value;
+  var selectedDay = document.getElementById("day").value;
+  var scheduleDiv = document.getElementById("schedule");
+
+  // Clear previous schedule
+  scheduleDiv.innerHTML = "";
+
+  if (section.toUpperCase() === "A") {
+      // Display schedule for section A based on elective and selected day
+      var filteredSchedule = getFilteredSchedule(elective, selectedDay);
+      displaySchedule(filteredSchedule);
+  } 
+  else if (section.toUpperCase() === "B") {
+      scheduleDiv.innerHTML = "Please Select the Section A";
+  }else {
+      scheduleDiv.innerHTML = "Does'nt Exists";
+  }
+}
+
+function getFilteredSchedule(elective, selectedDay) {
+  var electiveKey = Object.keys(schedules).find(key => key.toLowerCase() === elective.toLowerCase());
+
+  var schedule = electiveKey ? schedules[electiveKey][selectedDay] : [];
+  return schedule || [];
+}
+
+function displaySchedule(schedule) {
+  var scheduleDiv = document.getElementById("schedule");
+
+  for (var i = 0; i < schedule.length; i++) {
+      var entry = schedule[i];
+      var scheduleEntry = document.createElement("p");
+      scheduleEntry.textContent = entry.time + " " + entry.subject + " " + entry.venue;
+      scheduleDiv.appendChild(scheduleEntry);
+  }
+}
+
+// Sample schedules for MB and CTRSS electives
+// Add schedules similarly as done for ISA
+var mbSchedule = {};
+var ctrssSchedule = {};
+
+
+if (!localStorage.getItem('agreed')) {
+    openPopup();
+}
+
+// Function to open the popup
+function openPopup() {
+    document.getElementById('popup-container').style.display = 'flex';
+}
+
+// Function to close the popup and set the agreed status
+function closePopup() {
+    document.getElementById('popup-container').style.display = 'none';
+    localStorage.setItem('agreed', 'true');
+}
